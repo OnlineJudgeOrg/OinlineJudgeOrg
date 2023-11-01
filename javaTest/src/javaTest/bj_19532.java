dc2254db9e8d2f3032e34b9eeff206a0699c6483
jinsusong <iko153@naver.com>
2023-11-01 22:27:49 +0900

    백준 브론즈2 19532번 수학은 비대면 강의입니다.

---
+++ javaTest/src/javaTest/bj_19532.java
@@ -0,0 +1,25 @@
+package javaTest;
+
+import java.util.*;
+import java.io.*;
+
+public class bj_19532{
+    public static void main(String args[]) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	String[] inputs = br.readLine().split(" ");
+    	
+    	int a = Integer.parseInt(inputs[0]);
+    	int b = Integer.parseInt(inputs[1]);
+    	int c = Integer.parseInt(inputs[2]);
+    	int d = Integer.parseInt(inputs[3]);
+    	int e = Integer.parseInt(inputs[4]);
+    	int f = Integer.parseInt(inputs[5]);
+    	
+    	int y = ((c*d)-(a*f)) / ((b*d) - (a*e));
+    	int x = ((c*e)-(b*f)) / ((a*e) - (b*d));
+    	
+    	System.out.println(x +" " + y);
+    }
+}
+
