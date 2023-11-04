4dd69be3a05c07f6a02b42d39ab81707eb8a4757
jinsusong <iko153@naver.com>
2023-11-04 14:51:34 +0900

    실버5, 브루트포스, 영화감독 숌

---
+++ javaTest/src/javaTest/bj_1436.java
@@ -0,0 +1,45 @@
+package javaTest;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_1436 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		String target_str = "666";
+		int target_int = Integer.parseInt(target_str);
+		//int i = 0;
+		int result = 0;
+		
+		//System.out.println("n : " + n );
+		//System.out.println("cnt : " + cnt );
+		while(cnt != n) {
+			//System.out.println("loop : " + i);
+			if(target_str.contains("666")) {
+				//System.out.println("cnt 증가 **");
+				cnt++;
+				//System.out.println("target_str : " + target_str);
+				//System.out.println("target_int : " + target_int);
+				result = target_int;
+				
+			}
+			if(cnt == n) {
+				break;
+			}
+			target_int = target_int+1;
+			target_str = String.valueOf(target_int);
+			//i++;
+		}
+		System.out.println(target_int);
+
+	}
+
+}
+
