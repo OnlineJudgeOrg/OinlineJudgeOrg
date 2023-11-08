f409cd2eac6f240eb08b6840e531319c7a9c4d92
ShSeanLEE <autotic77@gmail.com>
2023-11-08 20:25:14 +0900

    git test4 sh

--- javaTest/src/javaTest/bj_1436.java
+++ javaTest/src/javaTest/bj_1436.java
@@ -11,8 +11,9 @@ public class bj_1436 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		// git test song 
-		// giy test2 song 
-		// git test3 song 
+		// git test2 song
+		// git test3 song
+		// git test4 sh
 		int cnt = 0; // 666이 포함된 숫자 
 		String target_str = "666";
 		int target_int = Integer.parseInt(target_str);
