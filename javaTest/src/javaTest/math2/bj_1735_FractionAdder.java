3f0188ab15457723360ffa28d707f52101c8e708
jinsusong <iko153@naver.com>
2023-11-19 21:54:07 +0900

    [실버3] 1735번, 분수 합 : 코드 수정

--- javaTest/src/javaTest/math2/bj_1735_FractionAdder.java
+++ javaTest/src/javaTest/math2/bj_1735_FractionAdder.java
@@ -18,50 +18,44 @@ public class bj_1735_FractionAdder {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String[] input1 = br.readLine().split(" ");
-		int numerator1 = Integer.parseInt( input1[0] );
-		int denominator1 = Integer.parseInt( input1[1] );
+		int A = Integer.parseInt( input1[0] );
+		int B = Integer.parseInt( input1[1] );
 		
 		String[] input2 = br.readLine().split(" ");
-		int numerator2 = Integer.parseInt( input2[0] );
-		int denominator2 = Integer.parseInt( input2[1] );
+		int C = Integer.parseInt( input2[0] );
+		int D = Integer.parseInt( input2[1] );
 		
-//		System.out.println("numerator1 : " + numerator1);
-//		System.out.println("numerator2 : " + numerator2);
-
-		numerator1 *= denominator2;
-		denominator1 *= denominator2;
+		// 분수의 합
+		int numerator = (A*D) + (B*C);
+		int denominator = B*C;
 		
-		numerator2 *= Integer.parseInt( input1[1] );
-		denominator2 *= Integer.parseInt( input1[1] );
-
-//		System.out.println("numerator1 : " + numerator1);
-//		System.out.println("numerator2 : " + numerator2);
-		
-		int numerator = numerator1 + numerator2;
-		int denominator = denominator1;
-//		System.out.println("gcd 전 : " );
-//		System.out.println("numerator : " + numerator);
-//		System.out.println("denominator : " + denominator);
-//		
+		// 기약분수로 만들기 위해 최대공약수를 구한다.
 		int gcd = GCD(numerator, denominator);
-//		System.out.println("gcd : " + gcd);
 		
+		// 기약분수로 변경
 		numerator /= gcd; 
 		denominator /= gcd;
-//		System.out.println("gcd 후 : " );
+		
+		// 결과 출력
 		System.out.println(numerator + " " + denominator);
 		
 	}
 	
+	// 유클리드 호제법
 	public static int GCD(int a, int b) {
 		
-		while(b != 0) {
-			int r = a%b;
-			a = b;
-			b = r;
-			
+		// a가 b보다 큰 경우에 대해 코드를 추가해야하나? 
+		
+		// while(b != 0) {
+		// 	int r = a%b;
+		// 	a = b;
+		// 	b = r;
+		// 	
+		// }
+		if (b == 0) {
+			return a;
 		}
-		return a;
+		return GCD(a, a%b);
 	}
 
 }
