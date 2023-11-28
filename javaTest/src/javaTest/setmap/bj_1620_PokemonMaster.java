8e973651af7edcd664da976452fc14617431beb5
jinsusong <iko153@naver.com>
2023-11-28 19:41:29 +0900

    실버4, 나는야 포켓몬 마스터 이다솜, Character.isDigit(tmp);

--- javaTest/src/javaTest/setmap/bj_1620_PokemonMaster.java
+++ javaTest/src/javaTest/setmap/bj_1620_PokemonMaster.java
@@ -18,36 +18,40 @@ public class bj_1620_PokemonMaster{
     	int n = Integer.parseInt(nm[0]);
     	int m = Integer.parseInt(nm[1]);
     	
+    	// 포멧몬 이름을 말하면 숫자를 반환할 map
     	HashMap<String,Integer> pokemonName = new HashMap<String, Integer>();
+    	// 번호를 부르면 포켓몬 이름을 반환할 map
     	HashMap<Integer,String> pokemonNumber = new HashMap<Integer, String>();
     	
+    	// 입력 받기
     	for(int i=0; i<n; i++) {
     		String name = br.readLine();
     		pokemonName.put(name, i+1);
     		pokemonNumber.put(i+1, name);
     	}
-    	System.out.println(pokemonName);
-    	System.out.println(pokemonNumber );
+    	
+//    	System.out.println(pokemonName);
+//    	System.out.println(pokemonNumber );
     	StringBuilder sb = new StringBuilder();
     	
     	for(int j=0; j<m; j++) {
     		String input = br.readLine();
-    		System.out.println("input : " + input);
+//    		System.out.println("input : " + input);
     		
     		boolean isNumber = isNumber(input);
-    		System.out.println("result : " + isNumber);
+//    		System.out.println("result : " + isNumber);
     		
     		if(isNumber) {// 숫자(포켓몬 번호)인 경우 
-    			pokemonNumber.get(input);
-    			sb.append(pokemonNumber.get(input)).append("\n");
+    			int idx = Integer.parseInt(input);
+    			pokemonNumber.get(idx);
+    			sb.append(pokemonNumber.get(idx)).append("\n");
     		}else {// 문자(포켓몬 이름)인 경우 
     			pokemonName.get(input);
     			sb.append(pokemonName.get(input)).append("\n");
     		}
-    		System.out.println(sb);
-    		
     		
     	}
+    	System.out.println(sb);
     }
     public static boolean isNumber(String input) {
     	boolean result = false;
