cdef06cc17fa210d9fb65a52ba2b062620e5f1c7
ShSeanLEE <autotic77@gmail.com>
2023-12-06 20:12:39 +0900

    Update 5 Annotation Week 6
    - Annotation added in detail.
    - BOJ1934 TreeSet speed test added.

--- javaTest/src/ShSeanLee/BOJ1269_SymmetricDifference.java
+++ javaTest/src/ShSeanLee/BOJ1269_SymmetricDifference.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-
+// 1269번 대칭차집합 성공(952ms)
 public class BOJ1269_SymmetricDifference {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +21,7 @@ public class BOJ1269_SymmetricDifference {
         // Hashset 만드는 풀이 - 952ms
         HashSet<Integer> s1 = new HashSet<>();
         HashSet<Integer> s2 = new HashSet<>();
-        HashSet<Integer> intersection = new HashSet<>();
+        HashSet<Integer> intersection = new HashSet<>(); // 교집합을 위한 빈 HashSet을 만든다.
 
         StringTokenizer setA = new StringTokenizer(br.readLine());
         for (int i = 0; i <s1Length; i++) {
@@ -33,10 +33,10 @@ public class BOJ1269_SymmetricDifference {
             s2.add(Integer.parseInt(setB.nextToken()));
         }
 
-        intersection.addAll(s1);
-        intersection.retainAll(s2);
-        s1.removeAll(intersection);
-        s2.removeAll(intersection);
+        intersection.addAll(s1); // s1을 담고
+        intersection.retainAll(s2); // s1과 s2의 교집합을 결국 담는다.
+        s1.removeAll(intersection); // s1의 차집합
+        s2.removeAll(intersection); // s2의 차집합
         System.out.println(s1.size() + s2.size());
 
 
