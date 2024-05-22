317680013c76a4f9922cc564f81d81a68a59cafa
jinsusong <iko153@naver.com>
2024-05-22 20:54:55 +0900

    2024.05.22 최단경로 시작 전 이전 기록 commit

--- javaTest/src/javaTest/dp/bj_24416_fibonacci.java
+++ javaTest/src/javaTest/dp/bj_24416_fibonacci.java
@@ -28,10 +28,11 @@ public class bj_24416_fibonacci {
 	public static int fibonacci(int n) {
 		int[] f = new int[n+1];
 		f[0] = f[1] = 1;
-		for(int i=3; i<=n; i++) {
+		for(int i=2; i<n; i++) {
 			cnt2++;
 			f[i] = f[i-1] + f[i-2];
 		}
+		
 		return f[n];
 	}
 
